b31986f8b1daabfa5a208ba284550c3873539f77
pyavchik <pyavchik@gmail.com>
2015-08-31 00:09:39 +0300

    added test framework

---
+++ test/src/main/java/com/becomejavasenior/core/web/Component.java
@@ -0,0 +1,22 @@
+package com.becomejavasenior.core.web;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by machine on 29.08.15.
+ */
+// component - link, button, page
+public abstract class Component<T extends Component<T>> {
+
+    protected WebDriver driver;
+
+    public Component(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public abstract boolean isAvailable();
+
+    public T waitUntilAvalible() {
+        return new Wait<T>().forComponent((T) this).toBeAvailable();
+    }
+}
---
+++ test/src/main/java/com/becomejavasenior/core/web/WebPage.java
@@ -0,0 +1,22 @@
+package com.becomejavasenior.core.web;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by pyavchik on 30.08.15.
+ */
+public abstract class WebPage<T extends WebPage<T>> extends Component<T>{
+
+    public WebPage(WebDriver driver){
+        super(driver);
+    }
+
+    public abstract T load();
+
+    public T loadAndWaitUntilAvailable(){
+        load();
+        waitUntilAvalible();
+        return waitUntilAvalible();
+    }
+
+}
