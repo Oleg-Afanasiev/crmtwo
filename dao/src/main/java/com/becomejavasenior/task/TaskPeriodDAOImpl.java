04adfc8f76b1a9091193a480c737e8556279f247
Dmytro Tsapko <tsapko.dmitry@gmail.com>
2015-09-09 14:53:43 +0300

    TaskPeriod DAO was implemented

--- dao/src/main/java/com/becomejavasenior/task/TaskPeriodDAOImpl.java
+++ dao/src/main/java/com/becomejavasenior/task/TaskPeriodDAOImpl.java
@@ -13,37 +13,59 @@ import java.util.Map;
  */
 public class TaskPeriodDAOImpl extends GenericDAO<TaskPeriod> implements TaskPeriodDAO {
 
+    String saveNewTaskPeriod =  "INSERT INTO crmtwo.crm.task_period (period_name)\n" +
+                                "VALUES (?) RETURNING period_id;";
+
+    String updateTaskPeriod =   "UPDATE crmtwo.crm.task_period SET (period_name)=(?)\n" +
+                                "WHERE period_id = ?;";
+
+
+    String getTaskPeriodById =  "SELECT period_id, period_name\n" +
+                                "FROM crmtwo.crm.task_period\n" +
+                                "WHERE period_id = ?;";
+
+    String deleteTaskPeriod =   "DELETE FROM crmtwo.crm.task_period\n" +
+                                "WHERE period_id = ?;";
+
+
     public TaskPeriodDAOImpl(Connection connection) {
         this.connection = connection;
     }
 
     @Override
     protected Map<String, String> getConfig() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected String getQueryForSaveOrUpdate(Long id) {
-        return null;
+        return id == null ? saveNewTaskPeriod : updateTaskPeriod;
     }
 
     @Override
     protected String getQueryForGetById() {
-        return null;
+        return getTaskPeriodById;
     }
 
     @Override
     protected String getQueryForDelete() {
-        return null;
+        return deleteTaskPeriod;
     }
 
     @Override
     protected void setParamsForSaveOrUpdate(PreparedStatement statement, TaskPeriod entity) throws SQLException {
-
+        Long id = entity.getId();
+        statement.setString(1, entity.getName());
+        if(id != null){
+            statement.setLong(2, id);
+        }
     }
 
     @Override
     protected TaskPeriod mapFieldsForGetById(ResultSet resultSet) throws SQLException, NoSuchFieldException, IllegalAccessException {
-        return null;
+        final TaskPeriod taskPeriod = new TaskPeriodImpl();
+        super.setPrivateField(taskPeriod, "id", resultSet.getLong("period_id"));
+        taskPeriod.setName(resultSet.getString("period_name"));
+        return taskPeriod;
     }
 }
