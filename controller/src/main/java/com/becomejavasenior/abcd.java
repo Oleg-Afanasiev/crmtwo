c7a30bb61590aaeb969a035c40d30e6b52ce0f0e
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-09-16 02:56:19 +0300

    abcd

--- controller/src/main/java/com/becomejavasenior/abcd.java
+++ controller/src/main/java/com/becomejavasenior/abcd.java
@@ -2,13 +2,40 @@ package com.becomejavasenior;
 
 import com.becomejavasenior.impl.DealImpl;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 class abcd {
     public static void main(String[] args) {
         DaoManager daoManager = DaoManager.getInstance();
         DealDAO dealDAO = daoManager.getDealDAO();
-        Deal deal = dealDAO.getById(1);
+        DealStatusDAO dealStatusDAO = daoManager.getDealStatusDAO();
+        UserDAO userDAO = daoManager.getUserDAO();
+
+        Deal deal = dealDAO.getById(7);
+        DealStatus dealStatus = dealStatusDAO.getById(2);
+        User user = userDAO.getById(7);
+
         //dealDAO.insertOrUpdate();
+        BigDecimal bd = new BigDecimal(750000);
+        long nowTime = System.currentTimeMillis();
+        Date nowDate = new Date(nowTime);
         Deal newDeal = new DealImpl();
+
+        newDeal.setBudget(bd);
+        newDeal.setCreated(nowDate);
+        newDeal.setUpdated(nowDate);
+        newDeal.setName("Новая сделка");
+        newDeal.setResponsibleUser(user);
+
+        newDeal.setDealStatus(dealStatus);
+
+        dealDAO.insertOrUpdate(newDeal);
+
+        System.out.println(deal);
         System.out.println(newDeal);
+        System.out.println(dealStatus);
+        System.out.println(user);
+
     }
 }
