d743d86bcd43a59731ffc3ae2e1501f7ffd8804f
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-09-17 22:14:41 +0300

    abcd.java

--- controller/src/main/java/com/becomejavasenior/abcd.java
+++ controller/src/main/java/com/becomejavasenior/abcd.java
@@ -9,32 +9,32 @@ class abcd {
     public static void main(String[] args) {
         DaoManager daoManager = DaoManager.getInstance();
         DealDAO dealDAO = daoManager.getDealDAO();
-        DealStatusDAO dealStatusDAO = daoManager.getDealStatusDAO();
-        UserDAO userDAO = daoManager.getUserDAO();
+        //DealStatusDAO dealStatusDAO = daoManager.getDealStatusDAO();
+        //UserDAO userDAO = daoManager.getUserDAO();
 
         Deal deal = dealDAO.getById(7);
-        DealStatus dealStatus = dealStatusDAO.getById(2);
-        User user = userDAO.getById(7);
+       // DealStatus dealStatus = dealStatusDAO.getById(2);
+        //User user = userDAO.getById(7);
 
         //dealDAO.insertOrUpdate();
-        BigDecimal bd = new BigDecimal(750000);
-        long nowTime = System.currentTimeMillis();
-        Date nowDate = new Date(nowTime);
-        Deal newDeal = new DealImpl();
+//        BigDecimal bd = new BigDecimal(750000);
+//        long nowTime = System.currentTimeMillis();
+//        Date nowDate = new Date(nowTime);
+//        Deal newDeal = new DealImpl();
 
-        newDeal.setBudget(bd);
-        newDeal.setCreated(nowDate);
-        newDeal.setUpdated(nowDate);
-        newDeal.setName("Новая сделка");
-        newDeal.setResponsibleUser(user);
+//        newDeal.setBudget(bd);
+//        newDeal.setCreated(nowDate);
+//        newDeal.setUpdated(nowDate);
+//        newDeal.setName("Новая сделка");
+//        newDeal.setResponsibleUser(user);
+//
+//        newDeal.setDealStatus(dealStatus);
 
-        newDeal.setDealStatus(dealStatus);
-
-        deal.setName("новая сделка");
+        deal.setName("nnn");
         dealDAO.insertOrUpdate(deal);
 
         System.out.println(deal);
-        System.out.println(newDeal);
+       // System.out.println(newDeal);
        // System.out.println(dealStatus);
         //System.out.println(user);
     }
