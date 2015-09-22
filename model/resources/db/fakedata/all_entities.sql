ab3d8cbf32c50754b2404ef9ce029d7fdde1018a
Andrey Radionov <andyomsk@gmail.com>
2015-09-22 22:45:35 +0300

    fakedata small fix

--- model/resources/db/fakedata/all_entities.sql
+++ model/resources/db/fakedata/all_entities.sql
@@ -64,25 +64,25 @@ INSERT INTO crm."user" (role_id, username, last_name, first_name, email, created
 VALUES (10, 'miev', 'Mironov', 'Evgeniy', 'miev@mail.ru', current_timestamp, current_timestamp, FALSE);
 
 
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\file1.txt', 'text/plain', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\spravka0.pdf', 'application/pdf', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\screenshot003.jpg', 'image/jpeg', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\screenshot9.png', 'image/png', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\config33.xml', 'text/xml', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\review.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\teachers.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\presentation.pptx', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\screencast.avi', 'video/avi', current_timestamp, current_timestamp);
-INSERT INTO crm.file (file_path, file_mime_type, created, updated)
+INSERT INTO crm.file (file_name, file_mime_type, created, updated)
 VALUES ('c:\files\video.mp4', 'video/mp4', current_timestamp, current_timestamp);
 
 
