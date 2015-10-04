67d4988d941303cc91e5c8bbf4ce662ae4fc3d4f
pyavchik <pyavchik@gmail.com>
2015-10-04 21:05:01 +0300

    refactored folders structure for tests

---
+++ controller/src/test/java/com/becomejavasenior/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package com.becomejavasenior.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by pyavchik.a on 26.09.15.
+ */
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp(){
+        driver = new FirefoxDriver();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
